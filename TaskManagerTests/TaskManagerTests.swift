c252ef372be6cb4ee0e76455f685eb4fd5ede174
yensepbay <yensepbay@gmail.com>
2024-10-16 16:11:17 +0500

    first commit

---
+++ TaskManagerTests/TaskManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerTests.swift
+//  TaskManagerTests
+//
+//  Created by Meirambek Yensepbay on 16.10.2024.
+//
+
+import Testing
+@testable import TaskManager
+
+struct TaskManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
